9e7eda098b48ffbfcde8756ebce9ae42ae8c9752
fa1seW <74900409+fa1seW@users.noreply.github.com>
2022-05-28 23:51:52 +0800

    Merge remote-tracking branch 'origin/master'

--- ds.py
+++ ds.py
@@ -44,16 +44,16 @@ class Array_Seq:
         self.build(A)
         return x
 
-    def insert_first(self, x):
+    def insert_first(self, x):  # O(n)
         self.insert_at(0, x)
 
-    def delete_first(self):
+    def delete_first(self):  # O(n)
         self.delete_at(0)
 
-    def insert_last(self, x):
+    def insert_last(self, x):  # O(n)
         self.insert_at(len(self), x)
 
-    def delete_last(self):
+    def delete_last(self):  # O(n)
         self.delete_at(len(self) - 1)
 
 
@@ -87,12 +87,12 @@ class Dynamic_Array_Seq(Array_Seq):
         self.A = A
         self._compute_bounds()
 
-    def insert_last(self, x):
+    def insert_last(self, x):  # O(1)
         self._resize(self.size + 1)
         self.A[self.size] = x
         self.size += 1
 
-    def delete_last(self):
+    def delete_last(self):  # O(1)
         self.A[self.size - 1] = None
         self.size -= 1
         self._resize(self.size - 1)
@@ -120,11 +120,17 @@ class Linked_List_Node:
     def __init__(self, x):
         self.item = x
         self.next = None
+        self.prev = None
 
     def later_node(self, i):
         if i == 0: return self
         assert self.next
         return self.next.later_node(i - 1)
+
+    def prev_node(self, i):
+        if i == 0: return self
+        assert self.prev
+        return self.prev.prev_node(i - 1)
 
 
 class Linked_List_Seq:
@@ -150,17 +156,17 @@ class Linked_List_Seq:
         node = self.head.later_node(i)
         return node.item
 
-    def set_at(self, i):
+    def set_at(self, i, x):
         node = self.head.later_node(i)
         node.item = x
 
-    def insert_first(self, x):
+    def insert_first(self, x):  # O(1)
         new_node = Linked_List_Node(x)
         new_node.next = self.head
         self.head = new_node
         self.size += 1
 
-    def delete_first(self):
+    def delete_first(self):  # O(1)
         x = self.head.item
         self.head = self.head.next
         self.size -= 1
@@ -190,3 +196,107 @@ class Linked_List_Seq:
 
     def delete_last(self):
         return self.delete_at(len(self) - 1)
+
+
+class Doubly_Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.item
+            node = node.next
+
+    def __reviter__(self):
+        node = self.tail
+        while node:
+            yield node.item
+            node = node.prev
+
+    def build(self, X):
+        for a in reversed(X):
+            self.insert_first(a)
+
+    def insert_first(self, x):
+        new_node = Linked_List_Node(x)
+        if self.size == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.size += 1
+            return
+        if self.size == 1:
+            new_node.next = self.tail
+            self.head = new_node
+            self.tail.prev = new_node
+            self.size += 1
+            return
+        new_node.next = self.head
+        self.head = new_node
+        new_node.next.prev = new_node
+        self.size += 1
+
+    def insert_last(self, x):
+        new_node = Linked_List_Node(x)
+        if self.size == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.size += 1
+            return
+        if self.size == 1:
+            self.head.next = new_node
+            self.tail = new_node
+            self.size += 1
+            return
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+        self.size += 1
+
+    def delete_first(self):
+        if self.size == 0:
+            return
+        x = self.head.item
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            self.size -= 1
+            return x
+        self.head = self.head.next
+        self.head.prev = None
+        self.size -= 1
+        return x
+
+    def delele_last(self):
+        if self.size == 0:
+            return
+        x = self.tail.item
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            self.size -= 1
+            return x
+        self.tail = self.tail.prev
+        self.tail.next = None
+        self.size -= 1
+        return x
+
+    def swap_ends(self):
+        if self.size == 0 or self.size == 1:
+            return
+        x = self.delele_last()
+        y = self.delete_first()
+        self.insert_last(y)
+        self.insert_first(x)
+
+    def shift_left(self, k):
+        if self.size == 0:
+            return
+        for i in range(k):
+            x = self.delete_first()
+            self.insert_last(x)
